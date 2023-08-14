f898a2d8cddf277d19509d2dd1ace9e3a20e14cf
AtanasovCode <a_ljupce@yahoo.com>
2023-08-14 11:07:42 +0200

    initial file setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  blue: "#0A327B",
+  red: "#F65552",
+  grayBlue900: "#1C202B",
+  grayBlue700: "#5E6778",
+  grayBlue500: "#939CAD",
+  grayBlue300: "#E5EFFA",
+  grayBlue100: "#DDE7EE",
+  snow: "#F7FAFD",
+  white: "#FFFFFF"
+}
+
+const App = () => {
+  return (
+    <Container></Container>
+  );
+}
+
+export default App;
+
+const Container = styled.div``;
